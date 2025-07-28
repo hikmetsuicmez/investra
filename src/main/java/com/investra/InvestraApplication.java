e67fcdcdd0e91fae06dbe67a0c67432a8e45fcff
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2025-07-28 09:45:12 +0300

    Spring Boot projesini, Maven kurulumu ve temel yapılandırmaya yapıldı.

---
+++ src/main/java/com/investra/InvestraApplication.java
@@ -0,0 +1,13 @@
+package com.investra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investra/InvestraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
